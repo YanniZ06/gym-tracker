f3320a21439dfd0b06301fa87c633eab96ffd376
YanniZ06 <102467588+YanniZ06@users.noreply.github.com>
2026-07-04 18:57:27 +0200

    PWA Support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Gym Tracker',
+        short_name: 'GymTracker',
+        start_url: '/gym-tracker/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#13111f',
+        icons: [
+          { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ],
+  base: '/gym-tracker/',
 })
